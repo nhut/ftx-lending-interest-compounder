be6849404057b0c62de28914e09ca9cd3dcddadd
Nhut Do <mr.nhut@gmail.com>
2021-11-28 01:09:29 +0200

    Add basic Spring boot project.

---
+++ src/main/java/com/github/nhut/ftxlendcompounder/FtxLendCompounderApplication.java
@@ -0,0 +1,12 @@
+package com.github.nhut.ftxlendcompounder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtxLendCompounderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtxLendCompounderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/nhut/ftxlendcompounder/FtxLendCompounderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nhut.ftxlendcompounder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtxLendCompounderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
