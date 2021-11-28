263dbb4f1a97ab5b515a50bb6715091a35c8047b
Nhut Do <mr.nhut@gmail.com>
2021-11-28 14:47:12 +0200

    Add new configurable key "app.ftx.startup.call-compound-interest" to control if it should call compound interest service.

--- src/main/java/com/github/nhut/ftxlendcompounder/SchedulerConfig.java
+++ src/main/java/com/github/nhut/ftxlendcompounder/SchedulerConfig.java
@@ -1,21 +1,36 @@
 package com.github.nhut.ftxlendcompounder;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 public class SchedulerConfig {
 
+    private boolean callCompoundInterestSchedulerAtStartup = false;
+
     private final CompoundInterestService compoundInterestService;
 
     @Autowired
-    public SchedulerConfig(final CompoundInterestService compoundInterestService) {
+    public SchedulerConfig(final CompoundInterestService compoundInterestService, @Value("${app.ftx.startup.call-compound-interest}") String callCompoundInterestSchedulerAtStartupS) {
         this.compoundInterestService = compoundInterestService;
+        this.callCompoundInterestSchedulerAtStartup =
+                StringUtils.hasLength(callCompoundInterestSchedulerAtStartupS) && Boolean.parseBoolean(callCompoundInterestSchedulerAtStartupS);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        if (callCompoundInterestSchedulerAtStartup) {
+            compoundLendingInterestScheduler();
+        }
     }
 
     @Scheduled(cron = "0 5 * ? * *") //Gets trigger at every hour and 5 minute time.
